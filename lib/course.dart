1e46655195b30edb501ce63e6c7f93abcb6b680c
SunMoon97 <f20220757@goa.bits-pilani.ac.in>
2024-02-13 00:21:44 +0530

    timetable generated

---
+++ lib/course.dart
@@ -0,0 +1,9 @@
+// TODO Implement this library.
+class Course {
+  final String name;
+  final String code;
+  final String day;
+  final String time;
+
+  Course(this.name, this.code, this.day, this.time);
+}
